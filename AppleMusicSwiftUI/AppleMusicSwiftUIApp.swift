7f0cdb001749072c6d7e706e934909fea5bcecf1
Artem Galay <iostema@mail.ru>
2022-11-25 00:07:37 +0300

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Артем Галай on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
