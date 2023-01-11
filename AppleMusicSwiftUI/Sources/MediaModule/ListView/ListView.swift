b589c1c913c017067f9014a2b4aeb8cca1d9c838
Artem Galay <iostema@mail.ru>
2023-01-11 13:10:18 +0300

    Change modules.

---
+++ AppleMusicSwiftUI/Sources/MediaModule/ListView/ListView.swift
@@ -0,0 +1,46 @@
+//
+//  ListView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Elena Noack on 27.08.22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    @State private var selectedItems : Set<String> = Set<String>()
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        List {
+                ForEach( ListModel.mocks, id:\.self) { media in
+                    MediaRowView(media: media)
+                        .listRowSeparator(.visible)
+                } .onMove(perform: move)
+        }
+        .environment(\.editMode, .constant(.active))
+        .listStyle(.plain)
+        .navigationTitle("Медиатека")
+        .navigationBarItems(trailing:
+                                Button(action: {
+            dismiss()
+        }) {
+            Text("Готово")
+                .foregroundColor(.red)
+        })
+        .navigationBarBackButtonHidden(true)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        ListModel.mocks.move(fromOffsets: source, toOffset: destination)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+            .environmentObject(ModelData())
+    }
+}
